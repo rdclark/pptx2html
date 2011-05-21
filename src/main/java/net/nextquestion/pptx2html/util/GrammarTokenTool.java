c083caa99a9ed5b11aafaceea8968275edf674ba
Richard Clark <rdclark@nextquestion.net>
2011-05-20 17:44:53 -0700

    Clean up headers

--- src/main/java/net/nextquestion/pptx2html/util/GrammarTokenTool.java
+++ src/main/java/net/nextquestion/pptx2html/util/GrammarTokenTool.java
@@ -1,17 +1,7 @@
 package net.nextquestion.pptx2html.util;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Writer;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.io.*;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
